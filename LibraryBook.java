d597bac76ffaa5c8692686304d28799d16d2e066
Jenny Chen <jchen50@stuy.edu>
2016-12-01 06:05:13 -0500

    Create LibraryBook.java

---
+++ LibraryBook.java
@@ -0,0 +1,47 @@
+abstract Class LibraryBook extends Book implements Comparable<LibraryBook>{
+
+public String callNumber;
+
+public LibraryBook (String a, String t, String i, String c){
+setAuthor(a);
+setTitle(t);
+setISBN(i);
+callNumber = c;
+}
+
+//Getter and Setter
+public void setCallNumber(String c){
+callNumber = c;
+}
+public String getCallNumber(){
+return callNumber;
+}
+
+//methods
+abstract void checkout (String patron, String due){
+]
+
+abstract void returned(){
+}
+
+abstract String circulationStatus(){
+}
+
+public int compareTo(LibraryBook b){
+if (this.callNumber.compareTo(b.callNumber) = 0){
+return 0;
+}
+else if (this.callNumber.compareTo(b.callNumber) < 0){
+return -1;
+}
+
+else {
+return 1;
+}
+}
+
+public String toString(){
+return circulationStatus() + callNumber;
+}
+
+}
