cb04c5996ca729205ef734a6ac5c78238621c4f0
Jenny Chen <jchen50@stuy.edu>
2016-12-12 06:24:49 -0500

    Update Book.java

--- 01LibraryBook/Book.java
+++ 01LibraryBook/Book.java
@@ -1,1 +1,49 @@
+//Copied from my previous work over after I learned how to make directories inside repository
 
+public class Book{
+
+private String author;
+private String title;
+private String ISBN;
+
+//Constructor
+public Book(){
+author = "someone";
+title = "sometitle";
+ISBN = "1234";
+}
+
+public Book (String a, String t, String i){
+author = a;
+title = t;
+ISBN = i;
+}
+
+//Getters and Setters
+public void setAuthor (String a){
+author = a;
+}
+
+public void setTitle (String t){
+title = t;
+}
+
+public void setISBN (String i){
+ISBN = i
+}
+
+public String getAuthor (){
+return author;
+}
+
+public String getTitle (){
+return title;
+}
+
+public String getISBM (){
+return ISBN;
+}
+
+public String toString(){
+return title + ", " + author + ", " + ISBN;
+}
