5fc7dfd0f202dfe28a6dc58d4cc3f683e56b905c
Jenny Chen <jchen50@stuy.edu>
2016-12-12 06:27:14 -0500

    Create LibraryBook.java

---
+++ 01LibraryBook/LibraryBook.java
@@ -0,0 +1,49 @@
+//Copied from my previous work over after I learned how to make directories inside repository
+
+abstract Class LibraryBook extends Book implements Comparable<LibraryBook>{
+
+public String callNumber;
+
+public LibraryBook (String a, String t, String i, String c){
+setAuthor(a);
+setTitle(t);
+setISBN(i);
+callNumber = c;
+}
+
+//Getter and Setter
+public void setCallNumber(String c){
+callNumber = c;
+}
+public String getCallNumber(){
+return callNumber;
+}
+
+//methods
+abstract void checkout (String patron, String due){
+]
+
+abstract void returned(){
+}
+
+abstract String circulationStatus(){
+}
+
+public int compareTo(LibraryBook b){
+if (this.callNumber.compareTo(b.callNumber) = 0){
+return 0;
+}
+else if (this.callNumber.compareTo(b.callNumber) < 0){
+return -1;
+}
+
+else {
+return 1;
+}
+}
+
+public String toString(){
+return title + author + ISBN + circulationStatus() + callNumber;
+}
+
+}
