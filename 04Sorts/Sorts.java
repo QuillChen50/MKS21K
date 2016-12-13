c46cadd941ad01d86e4de023e15b4a75caa4183b
Jenny Chen <jchen50@stuy.edu>
2016-12-13 15:34:10 -0500

    Update Sorts.java

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -1,1 +1,21 @@
+public class Sorts{
+  
+public static String name(){
+    return "01.Last.First"; 
+  }
+ 
+public static void selectionSort(int[] data){  
+    int need2Sort;
+    int bestVal;
+    need2Sort = data.length;
+    bestVal = data[0]
+  for (int i = 0; i < data.length; i++){
+    for (int j = 0; j < data.length - 1){
+    if (bestVal > data[j]){
+      bestVal = data[i];}
+    
+      
+  }
 
+  
+}
