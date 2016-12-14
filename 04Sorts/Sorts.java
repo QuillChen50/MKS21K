c0b073117c8b19ea2796eb51abc53c0bd8588f0e
Jenny Chen <jchen50@stuy.edu>
2016-12-14 15:35:51 -0500

    Update Sorts.java

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -8,14 +8,28 @@ public static void selectionSort(int[] data){
     int need2Sort;
     int bestVal;
     need2Sort = data.length;
-    bestVal = data[0]
-  for (int i = 0; i < data.length; i++){
-    for (int j = 0; j < data.length - 1){
-    if (bestVal > data[j]){
+  for  
+  for (int i = 0; i < data.length() - 1; i--){
+    if (bestVal > data[i]){
       bestVal = data[i];}
     
       
   }
+    
+    
 
-  
+public static void insertionSort(int[] data){    
+  for (int i = 0; i < data.length; i++){
+    if (data[i] > data[i + 1]) {
+      int temp = data[i];
+      data[i] = data[i+1];
+      data[i+1] = temp;     
+                               }
+                                       
+  else 
+  {for }
+                                            }
+    
+    
+    
 }
