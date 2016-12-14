4f8ea09f9aa2a32050a7b7c69afdfadd86f8e52d
QuillChen50 <jchen50@stuy.edu>
2016-12-14 16:53:11 -0500

    More Changes To Loop

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -8,9 +8,9 @@ public static void selectionSort(int[] data){
     int bestValIndex;
     int bestVal;
     bestValIndex = 0;
-    bestVal = data[data.length()]
-	for (int p = 0; p < data.length(); p++){
-	    for (int i = 1; i < data.length() - 1; i++){
+    bestVal = data[0]
+	for (int p = 0; p < data.length(); p++){ //loop that takes each number from the data
+	    for (int i = 1; i < data.length() - 1; i++){ //loop comparing that # with those after it
 	    if (data[i] < bestVal){
 		bestVal = data[i];
 	        bestValIndex = i;}
@@ -23,17 +23,20 @@ public static void selectionSort(int[] data){
     
     
 
-public static void insertionSort(int[] data){    
-
-    
-	for (int p = 0; p < data.length(); p++){
-	    for (int i = 1; i < data.length() - 1; i++){  
-		if (data[i] > data[i + 1]) {
-		    int temp = data[i];
-		    data[i] = data[i+1];
-		    data[i+1] = temp;     
+public static void insertionSort(int[] data){
+ 
+ int bestValIndex;
+ int bestVal;
+ bestValIndex = 0;
+ bestVal = data[0]
+  
+	for (int p = 1; p < data.length(); p++)  
+		if (data[p] < bestVal) {
+		    bestValIndex = p;
+		    bestVal = data[p]
+		while  (bestVal <= data[bestValIndex - i)
+                               }
                                             }
-                                                       }
     
     
     
