a7bd90cf472d4d0c2019ad2459ca241ec110c28a
Jenny Chen <jchen50@stuy.edu>
2017-01-02 23:49:24 -0500

    can't quite understand why the same 4 lines are getting error messages. Will come back to it later

--- CirculatingBook.java
+++ CirculatingBook.java
@@ -1,17 +1,14 @@
 class CirculatingBook extends LibraryBook{
 
-public String currentHolder;
-public String dueDate;
+    //maybe should make these private
+ String currentHolder;
+ String dueDate;
 
 public CirculatingBook(String a, String t, String i, String c){
-    /*Originally thought it would be like this but then realized I could use super
-setAuthor(a);
-setTitle(t);
-setISBN(i);
-callNumber = c;*/
+    // Originally thought it would be like this but then realized I could use super
+    super(a, t, i, c);
 currentHolder = "";
 dueDate = "";
-super(a,t,i,c)
     
 }
 
@@ -20,28 +17,57 @@ public void checkout (String patron, String due){
 setDueDate(due);
 }
 
-
     //now for some set methods
 
     public setDueDate(String date){
 	dueDate = date;}
     public setCurrentHolder (String Holder){
 	currentHolder = Holder;}
-    
+
+    //now as for get
+
+    public String getDueDate(){
+	return dueDate;
+    }
+    public String getCurrentHolder(){
+	return currentHolder;}
+
+    //since if returned, no more current holder or due date
 public void returned(){
-currentHolder = "";
-dueDate = "";
+    setCurrentHolder("");
+    setDueDate("");
 }
-  
-public String circulation(){
+
+public String circulationStatus(){
   if (currentHolder == ""){
 return currentHolder + dueDate + "book available on shelves";}
   else {
-return currentHolder + dueDate + "book not available";
+      return currentHolder + dueDate + "book not available";}
+}
 
 public String toString(){
 return title + author + ISBN + circulation()
 }
     
 }
+
+/*
+
+$ javac CirculatingBook.java
+CirculatingBook.java:33: error: illegal start of expression
+public String toString(){
+^
+CirculatingBook.java:33: error: ';' expected
+public String toString(){
+                      ^
+CirculatingBook.java:34: error: ';' expected
+return title + author + ISBN + circulation()
+                                            ^
+CirculatingBook.java:37: error: reached end of file while parsing
+}
+ ^
+4 errors
+
+
+ */
 
