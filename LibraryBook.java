9a8c2b8e9d2247065b6aaa3532930512c4fab2af
Jenny Chen <jchen50@stuy.edu>
2017-01-03 00:00:09 -0500

    edited my previous code. However, still some problems with my classes

--- LibraryBook.java
+++ LibraryBook.java
@@ -3,10 +3,14 @@ abstract Class LibraryBook extends Book implements Comparable<LibraryBook>{
 public String callNumber;
 
 public LibraryBook (String a, String t, String i, String c){
+    /*replacing this with super; the entire point of the assignment
 setAuthor(a);
 setTitle(t);
 setISBN(i);
-callNumber = c;
+    */
+    super (a, t, i);
+    //at first I was curious as to how having missing inputs that should have been required in the super constructor would still work?
+    setCallNumber(c);
 }
 
 //Getter and Setter
@@ -18,6 +22,7 @@ return callNumber;
 }
 
 //methods
+//was wondering whether must make these public
 abstract void checkout (String patron, String due){
 ]
 
@@ -28,6 +33,8 @@ abstract String circulationStatus(){
 }
 
 public int compareTo(LibraryBook b){
+    //However, I realized that all of this could be replaced by a simple use of comparTo
+    /*
 if (this.callNumber.compareTo(b.callNumber) = 0){
 return 0;
 }
@@ -38,10 +45,13 @@ return -1;
 else {
 return 1;
 }
+    */
+    return callNumber.compareTo(b.callNumber);
 }
 
 public String toString(){
-return title + author + ISBN + circulationStatus() + callNumber;
+    //replacing part of my original code with super
+return super.toString + circulationStatus() + callNumber;
 }
 
 }
