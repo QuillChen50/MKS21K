b49e4313250083f5f5d26a2ad8a3c81bd5625447
Jenny Chen <jchen50@stuy.edu>
2017-01-03 00:14:55 -0500

    changes to SuperArrayIterator, need to fix up and add exceptions later. Missing methods as well

--- 02SuperArrayIterator/SuperArrayIterator.java
+++ 02SuperArrayIterator/SuperArrayIterator.java
@@ -1,1 +1,49 @@
+import java.util.Iterator;
 
+public class SuperArray implements Iterable<String>{
+  private String[] data;
+  private int size;
+
+
+    //constructors
+
+    //by default, there's 10 space
+    public SuperArray(){
+    size = 0;
+    data = new String[10];
+    }
+    //if want to change capacity, use following. Be aware of negative capacity
+    //I want to prevent setting a 0 or negative array since if 0, then just default
+    public (int capac){
+	if (capac <= 0){
+	    throw new IllegalArgumentException();
+	}
+	size = capac;
+	data = new String[capac];
+    }
+
+    //this can be used to prevent losing orginal value
+    public String get(int index){
+	return data[index];
+    }
+
+    public String set(int index, String element){
+	String temp = get(index);
+	data[index] = element;
+	return orig;
+    
+    //Help to avoid messing with it
+    public int size(){
+	return size;
+    }
+
+    public void clear(){
+    size = 0;
+    }
+    public boolean isEmpty(){
+    return size() == 0;
+    }
+    
+
+    
+}
